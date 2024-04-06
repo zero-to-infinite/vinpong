f00e08f81c5577158947ec3ae0c0005a46d3509c
okxooxoo <okxooxoo@naver.com>
2024-04-06 16:19:28 +0900

    create react native project

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from '../styles/HomeStyles';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>빈티지핑퐁</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
